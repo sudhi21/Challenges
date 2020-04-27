efd9708fefb598f50b5e874d92ad85892f30d3d8
sudhi21 <34230574+sudhi21@users.noreply.github.com>
2020-04-27 16:06:10 +0530

    Add files via upload

---
+++ hungrybot.php
@@ -0,0 +1,15 @@
+<?php
+$cookie_name = "FLAG";
+$cookie_value = "aW5jdGZqe2gzcjNfaTVfeTA0cl9jMDBraTN9";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+<html>
+<head>
+    <title>Hungry Bot!</title>
+</head>
+<body style = "background-color: peachpuff" >
+     <p style = "font-style: italic;color:royalblue;font-family:Times;font-size: 1cm">I'm damn hungry,get me something to eat!</p>
+
+
+</body>
+</html>
