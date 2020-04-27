7b5ef15d2074bf8cfc9d4c3c8979aa237dfcf808
sudhi21 <34230574+sudhi21@users.noreply.github.com>
2020-04-27 16:07:06 +0530

    Add files via upload

---
+++ mock2.php
@@ -0,0 +1,23 @@
+<?php
+$flag = "inctfj{y0ur_c00ki3_i5_D3lici0u5}";
+
+
+if(isset($_COOKIE['HungerBot2']) && $_COOKIE['HungerBot2'] == "aW5jdGZqe2gzcjNfaTVfeTA0cl9jMDBraTN8"){
+
+    echo $flag;
+}
+
+
+
+
+?>
+<html>
+<head>
+    <title>Hungry Bot 2!</title>
+</head>
+<body style = "background-color: peachpuff" >
+     <p style = "font-style: italic;color:royalblue;font-family:Times;font-size: 1cm">This time ,I need you to add in some cookies;/</p>
+
+<! Add cookie with name "HungerBot2" and value "aW5jdGZqe2gzcjNfaTVfeTA0cl9jMDBraTN8" -- >
+</body>
+</html>
